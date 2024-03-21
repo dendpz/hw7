4b2069b33f5af08d6d295ac5c372595d43701f71
Dennis <depazdennis@gmail.com>
2024-03-21 01:29:09 -0700

    v3

--- js/ex1.js
+++ js/ex1.js
@@ -5,50 +5,43 @@ const questions = [
     answer: "2+2 = 4"
   },
   {
-    statement: "In which year did Christopher Columbus discover America?",
+    statement: "In what year did Christopher Columbus discover America?",
     answer: "1492"
   },
   {
-    statement:
-      "What occurs twice in a lifetime, but once in every year, twice in a week but never in a day?",
+    statement: "What occurs twice in a lifetime, but once in every year, twice in a week but never in a day?",
     answer: "The E letter"
   }
 ];
 
-let i = 1; // Question index
+let i = 1;
 
 questions.forEach(question => {
-  // Question title
-  const titleElement = document.createElement("strong");
-  titleElement.textContent = `Question ${i}: `;
+  const qNum = document.createElement("b");
+  qNum.textContent = `Question ${i}: `;
 
-  // Question statement text in a <i> tag
-  const statementTextElement = document.createElement("i");
-  statementTextElement.textContent = question.statement;
+  const qText = document.createElement("i");
+  qText.textContent = question.statement;
 
-  // Question statement in a <div> tag
-  const statementElement = document.createElement("div");
-  statementElement.appendChild(titleElement);
-  statementElement.appendChild(statementTextElement);
+  const qElement = document.createElement("div");
+  qElement.appendChild(qNum);
+  qElement.appendChild(qText);
 
-  // Answer zone, with a button initially
-  const answerZoneElement = document.createElement("div");
+  const answerElement = document.createElement("div");
   const buttonElement = document.createElement("button");
   buttonElement.textContent = "Show answer";
-  answerZoneElement.appendChild(buttonElement);
+  answerElement.appendChild(buttonElement);
 
   buttonElement.addEventListener("click", () => {
-    // Replace button by answer
-    const answerElement = document.createElement("div");
-    answerElement.textContent = question.answer;
-    answerZoneElement.innerHTML = "";
-    answerZoneElement.appendChild(answerElement);
+    const showElement = document.createElement("div");
+    showElement.textContent = question.answer;
+    answerElement.innerHTML = "";
+    answerElement.appendChild(showElement);
   });
-
-  // Question is statement + answer
+  
   const questionElement = document.createElement("p");
-  questionElement.appendChild(statementElement);
-  questionElement.appendChild(answerZoneElement);
+  questionElement.appendChild(qElement);
+  questionElement.appendChild(answerElement);
   document.getElementById("content").appendChild(questionElement);
 
   i++;
