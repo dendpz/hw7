5be4208ecaade4aafe0d92ddb327e621a1ed5e0d
Dennis <depazdennis@gmail.com>
2024-03-21 02:20:29 -0700

    v5

--- js/ex1.js
+++ js/ex1.js
@@ -14,32 +14,34 @@ const questions = [
   }
 ];
 
+// To number the questions
 let i = 1;
 
+// For-each for our questions list
 questions.forEach(question => {
-  const qNum = document.createElement("b");
+  const qNum = document.createElement("b"); // Bolds the "Question x"
   qNum.textContent = `Question ${i}: `;
 
-  const qText = document.createElement("i");
+  const qText = document.createElement("i"); // Italicizes the actual question text
   qText.textContent = question.statement;
 
-  const qElement = document.createElement("div");
+  const qElement = document.createElement("div"); // Create div element to include both question# and text
   qElement.appendChild(qNum);
   qElement.appendChild(qText);
 
-  const answerElement = document.createElement("div");
-  const buttonElement = document.createElement("button");
+  const answerElement = document.createElement("div"); 
+  const buttonElement = document.createElement("button"); // Create a "Show answer" button and add it to the answerElement
   buttonElement.textContent = "Show answer";
   answerElement.appendChild(buttonElement);
 
-  buttonElement.addEventListener("click", () => {
+  buttonElement.addEventListener("click", () => { // Add an event that when clicked, the answer from the question will show and the button will disappear
     const showElement = document.createElement("div");
     showElement.textContent = question.answer;
     answerElement.innerHTML = "";
     answerElement.appendChild(showElement);
   });
   
-  const questionElement = document.createElement("p");
+  const questionElement = document.createElement("p"); // Add all together
   questionElement.appendChild(qElement);
   questionElement.appendChild(answerElement);
   document.getElementById("content").appendChild(questionElement);
